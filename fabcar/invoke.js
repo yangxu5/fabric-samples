3944d0c617d253b300b69b2264ee99f07aab4d0e
yangxu5 <40454856+yangxu5@users.noreply.github.com>
2018-07-19 10:22:19 +0800

    Update invoke.js

--- fabcar/invoke.js
+++ fabcar/invoke.js
@@ -112,7 +112,7 @@ Fabric_Client.newDefaultKeyValueStore({ path: store_path
 			let handle = setTimeout(() => {
 				event_hub.disconnect();
 				resolve({event_status : 'TIMEOUT'}); //we could use reject(new Error('Trnasaction did not complete within 30 seconds'));
-			}, 3000);
+			}, 30000);
 			event_hub.connect();
 			event_hub.registerTxEvent(transaction_id_string, (tx, code) => {
 				// this is the callback for transaction event status
